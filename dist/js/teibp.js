33d558c83abe6628358da42cceae826355dd75bf
Dareia <d.d.kondakova@gmail.com>
2016-12-18 15:49:40 +0300

    page breaks appear when toolbox unchecked

--- dist/js/teibp.js
+++ dist/js/teibp.js
@@ -3,10 +3,10 @@ function clearPageBreaks() {
     for (var e = document.querySelectorAll(".-teibp-pb"), t = 0; t < e.length; t++) e[t].style.display = "none"
 }
 
-/* function addPageBreaks() {
+ function addPageBreaks() {
     for (var e = document.querySelectorAll("pb"), t = 0; t < e.length; t++) e[t].style.display = "inline";
     for (var e = document.querySelectorAll(".-teibp-pb"), t = 0; t < e.length; t++) e[t].style.display = "inline"
-} */
+} 
 
 function init() {
     var e = document.getElementById("pbToggle");
